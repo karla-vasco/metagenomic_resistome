796023bb94ca59482868264bb4bf7755af1c3b4e
karla-vasco <80479988+karla-vasco@users.noreply.github.com>
2021-04-22 13:23:27 -0400

    Update 1-deepARG_install.sh

--- scripts/deeparg/1-deepARG_install.sh
+++ scripts/deeparg/1-deepARG_install.sh
@@ -1,10 +1,32 @@
+#!/bin/bash --login  
+ 
+########## Define Resources Needed with SBATCH Lines ##########
+#SBATCH --job-name=DeepARG_install# give your job a name for easier identification (same as -J)
+#SBATCH --time=168:00:00 # limit of wall clock time - how long will the job take to run? (same as -t)
+#SBATCH --ntasks=1     # number of tasks - how many tasks (nodes) does your job require? (same as -n)
+#SBATCH --cpus-per-task=4 # number of CPUs (or cores) per task (same as -c)
+#SBATCH --mem=50G     # memory required per node - amount of memory (in bytes)
+#SBATCH --output=/mnt/scratch/vascokar/mastitis_study/eofiles/DeepARG_install.%j.out #Standard output
+#SBATCH --error=/mnt/scratch/vascokar/mastitis_study/eofiles/DeepARG_install.%j.err #Standard error log
+
+########## Diplay the job context ######
+echo Job: $SLUM_JOB_NAME with ID $SLURM_JOB_ID
+echo Running on host `hostname`
+echo Job started at `date '+%T %a %d %b %Y'`
+echo Directory is `pwd`
+echo Using $SLURM_NTASKS processors across $SLURM_NNODES nodes
+
+######### Assign path variables ########
+
+INSTALL_DIRECTORY=/mnt/home/vascokar/mastitis_study/bin
+
 #Installation manual in https://bitbucket.org/gusphdproj/deeparg-ss/src/master/
 
 #Create a new folder to install deepARG
 
-cd /mnt/home/vascokar/mastitis_study/bin/
-mkdir deepARG
-cd deepARG
+cd $INSTALL_DIRECTORY
+mkdir deeparg
+cd deeparg
 
 #Create a virtual environment with virtualenv
 
@@ -17,7 +39,7 @@ module load Python/2.7.18
 
 #Install deeparg with pip and download the data required by deeparg
 pip install deeparg==1.0.2
-deeparg download_data -o /mnt/home/vascokar/mastitis_study/bin/deeparg
+deeparg download_data -o $INSTALL_DIRECTORY/deeparg
 
 ###It took foreeeever to download data (about 6 hours)
 
