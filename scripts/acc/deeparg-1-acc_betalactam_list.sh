ce54bef39495ae41126bfde9316620a882919828
karla-vasco <80479988+karla-vasco@users.noreply.github.com>
2021-07-21 21:57:57 -0400

    Update deeparg-1-acc_betalactam_list.sh

--- scripts/acc/deeparg-1-acc_betalactam_list.sh
+++ scripts/acc/deeparg-1-acc_betalactam_list.sh
@@ -8,7 +8,7 @@ for f in *contigs_deeparg.mapping.ARG # for each sample f
 do
   n=${f%%contigs_deeparg.mapping.ARG} # strip part of file name
 
-awk '/beta-lactam/{print $4}' ${n}contigs_deeparg.mapping.ARG > $OUTPUT_DIRECTORY/${n}beta-lactam_ARG_contigs.lst
+awk '/beta-lactam/{print $4}' $INPUT_DIRECTORY/${n}contigs_deeparg.mapping.ARG > $OUTPUT_DIRECTORY/${n}beta-lactam_ARG_contigs.lst
 
 awk '/beta-lactam/{print $4}' $INPUT_DIRECTORY/${n}contigs_deeparg.mapping.potential.ARG > $OUTPUT_DIRECTORY/${n}beta-lactam_potentialARG_contigs.lst
 
